ecd0d0b47dd578750beef41fe09e1e6930ea38f5
jdw5 <87954417+jdw5@users.noreply.github.com>
2025-06-08 05:19:41 +0000

    Fix styling

--- workbench/app/Models/User.php
+++ workbench/app/Models/User.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Workbench\App\Models;
 
-use Honed\Action\Attributes\ActionGroup;
 use Honed\Action\Attributes\UseActions;
 use Honed\Action\Concerns\HasActions;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
